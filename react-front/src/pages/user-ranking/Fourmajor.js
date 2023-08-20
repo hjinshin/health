fd9a7f5c66ce9d5cf4850751fa86139a7e57830c
hjinshin <gudwls818@gmail.com>
2023-08-20 18:03:28 +0900

    4대운동 버튼추가

--- react-front/src/pages/user-ranking/Fourmajor.js
+++ react-front/src/pages/user-ranking/Fourmajor.js
@@ -5,12 +5,13 @@ import './UserRank.css';
 
 function Fourmajor(props) {
     const category = '4-major';
+    const [subcategory, setSubcategory] = useState('sum');
     const [userList, setUserList] = useState([]);
 
     useEffect(() => {
         async function fetchUserList () {
             try {
-                const res = await axios.get(`/api/user-ranking?category=${category}&subcategory=`);
+                const res = await axios.get(`/api/user-ranking?category=${category}&subcategory=${subcategory}`);
                 // 데이터에 순위 부여
                 const dataWithRanking = res.data.map((data, rank) =>(
                     {...data, rank: rank+1}
@@ -22,10 +23,17 @@ function Fourmajor(props) {
         } 
 
         fetchUserList();       
-    }, []);
+    }, [category, subcategory]);
 
     return (
         <div className='tier-list-container'>
+            <div className='subcategory-button-container'>
+                <button className='subcategory-button' onClick={()=>setSubcategory('sum')}>합계</button>
+                <button className='subcategory-button' onClick={()=>setSubcategory('bench')}>벤치</button>
+                <button className='subcategory-button' onClick={()=>setSubcategory('dead')}>데드</button>
+                <button className='subcategory-button' onClick={()=>setSubcategory('squat')}>스쿼트</button>
+                <button className='subcategory-button' onClick={()=>setSubcategory('milpress')}>밀프</button>
+            </div>
             <div style={{'height': '50px'}}></div>
             <Table data={userList} />
         </div>
