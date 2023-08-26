a2556ffd704f1f674e3ad7947657de9c54b64a05
hjinshin <gudwls818@gmail.com>
2023-08-26 21:23:34 +0900

    merge database

--- react-front/src/pages/user-ranking/Fourmajor.js
+++ react-front/src/pages/user-ranking/Fourmajor.js
@@ -42,25 +42,12 @@ function Fourmajor(props) {
 
     return (
         <div className='tier-list-container'>
-<<<<<<< HEAD
-
-            <div className='subcategory-button-container'>
-                <button className='subcategory-button' onClick={()=>setSubcategory('sum')}>합계</button>
-                <button className='subcategory-button' onClick={()=>setSubcategory('bench')}>벤치</button>
-                <button className='subcategory-button' onClick={()=>setSubcategory('dead')}>데드</button>
-                <button className='subcategory-button' onClick={()=>setSubcategory('squat')}>스쿼트</button>
-                <button className='subcategory-button' onClick={()=>setSubcategory('milpress')}>밀프</button>
-            </div>
-            <div style={{'height': '50px'}}></div>
-            <Table data={userList} />
-=======
             <div className='subcategory-button-container'>
                 {buttons.map((button) => (
                     <button className='subcategory-button' key={button.id} onClick={()=>setSubcategory(button.type)}>{button.label}</button>    
                 ))}
             </div>
             <Table data={userList} onSubmit={props.onSubmit}/>
->>>>>>> database
         </div>
     );
 }
