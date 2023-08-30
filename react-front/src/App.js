36ee10a3c41a3942206fec9ef5eba87ee7d6711a
hjinshin <gudwls818@gmail.com>
2023-08-30 20:42:04 +0900

    토큰 검증

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -3,9 +3,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Home from "./pages/home/Home"
+import { useEffect } from 'react';
+import axios from 'axios';
 //import Footer from "./components/Footer"
 
 function App() {
+  useEffect(() => {
+    async function login() {
+      await axios({
+          method: "GET",
+          url: `/api/access-token`,
+          headers: {
+              "Content-Type": "application/json;charset=utf-8"
+          }
+      }).then((res) => {
+          console.log(res);
+      });
+  };
+  login();
+  }, []);
+  
   return (
     <BrowserRouter>
       <div className="App">
