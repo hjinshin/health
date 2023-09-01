40659e7efaf41630458daf51e7f501da0336eb1c
hjinshin <gudwls818@gmail.com>
2023-09-02 00:39:58 +0900

    test

--- springboot-back/src/main/java/com/health/springbootback/controller/LoginController.java
+++ springboot-back/src/main/java/com/health/springbootback/controller/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController {
 
     @GetMapping("/auth/kakao/callback")
     public ResponseEntity<LoginResponseDto> kakaoCallback(@RequestParam String code){
-        //System.out.println("카카오 인가코드: " + code);
+        System.out.println("카카오 인가코드: " + code);
         String kakaoAccessToken = authService.getKakaoAccessToken(code);
         System.out.println("카카오 엑세스토큰: " + kakaoAccessToken);
 
