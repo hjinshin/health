b4251604c1d8287609e588c656cb6263cd014609
hjinshin <gudwls818@gmail.com>
2023-09-03 05:22:45 +0900

    이미지 업로드

--- react-front/src/pages/mypage/ChangeInfo.js
+++ react-front/src/pages/mypage/ChangeInfo.js
@@ -1,12 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef } from 'react';
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import './ChangeInfo.css'
 
 const SERVER_SEARCH_URL = process.env.REACT_APP_SPRINGBOOT_BACK_URL;
 function ChangeInfo(props) {
     const [nickname, setNickname] = useState(sessionStorage.getItem('nickname')); // 초기값은 빈 문자열
-    const [img, setImg] = useState();
+    const [img, setImg] = useState({
+        url: undefined,
+        tempData: undefined,
+        origin: undefined
+    });
     const navigate = useNavigate();
+    const imageInputRef = useRef(null);
 
     useEffect(() => {
         async function loadImg() {
@@ -20,39 +26,67 @@ function ChangeInfo(props) {
             }).then((res) => {
                 const blob = new Blob([res.data], { type: 'image/png' });
                 const imageUrl = URL.createObjectURL(blob);
-                setImg(imageUrl);
+                setImg((prev) => {
+                    return {...prev, url: imageUrl, origin: imageUrl};
+                });
             });
            }
            loadImg();
     }, []);
 
+    function resetFileInput() {
+        if(imageInputRef.current) 
+            imageInputRef.current.value = '';
+    }
+    function onClickImageUpload() {
+        imageInputRef.current.click();
+    }
+    
     
     function handleChangeFile(e) {
         const maxSize = 100 * 1024;
         const selectedImg = e.target.files[0];
-        if(selectedImg.size > maxSize)
-            alert("이미지 용량이 너무 큽니다(최대 100KB)");
-        else
-            sendImg(e.target.files[0]);
+        if(selectedImg !== undefined) {
+            if(selectedImg.size > maxSize) 
+                alert("이미지 용량이 너무 큽니다(최대 100KB)");
+            else { 
+                setImg((prev) => {
+                    return {...prev, tempData: selectedImg};
+                });
+                const blob = new Blob([selectedImg], { type: 'image/png' });
+                const imageUrl = URL.createObjectURL(blob);
+                setImg((prev) => {
+                    return {...prev, url: imageUrl};
+                });
+            }           
+        } else {
+            setImg((prev) => {
+                return {...prev, tempData: undefined, url: img.origin};
+            });            
+        }
       }
      
       async function sendImg(file){
-        const fd = new FormData();
-        fd.append("file", file);
-        console.log(fd);
-        axios.post(SERVER_SEARCH_URL + '/api/image', fd, {
-          headers: {
-            "Authorization": sessionStorage.getItem('Authorization'),
-            "Content-Type": `multipart/form-data`,
-          }
-        })
-        .then((res) => {
-            const blob = new Blob([file], { type: 'image/png' });
-            const imageUrl = URL.createObjectURL(blob);
-            setImg(imageUrl);
-            window.location.reload();
-        })
+        if(file !== undefined) {
+            const fd = new FormData();
+            fd.append("file", file);
+            axios.post(SERVER_SEARCH_URL + '/api/image', fd, {
+            headers: {
+                "Authorization": sessionStorage.getItem('Authorization'),
+                "Content-Type": `multipart/form-data`,
+            }
+            })
+            .then((res) => {
+                const blob = new Blob([file], { type: 'image/png' });
+                const imageUrl = URL.createObjectURL(blob);
+                setImg((prev) => {
+                    return {...prev, url: imageUrl, tempData: undefined, origin: imageUrl};
+                });
+            })            
+        }
+        resetFileInput();
       }
+
     async function fetchNick () {
         await axios({
             method: "PUT",
@@ -62,9 +96,7 @@ function ChangeInfo(props) {
                 "Authorization": sessionStorage.getItem('Authorization'),
 
             },
-            params: {
-                "nickname": nickname
-            }
+            params: { "nickname": nickname }
         }).then((res) => {
             console.log(res.data);
             if(res.data.success){
@@ -75,18 +107,15 @@ function ChangeInfo(props) {
             else{
                 alert(res.data.message);
             }
-            window.location.reload();
         });
     };
 
     const handleChangeNickname = () => {;
         // 입력된 값을 닉네임 상태로 설정
-        if(nickname.length <2 || nickname.length >21){
+        if(nickname.length <2 || nickname.length >21)
             alert("닉네임은 최소 3글자 및 최대 20글자까지 가능합니다.")
-        }
-        else{
+        else
             fetchNick();            
-        }
     };
 
     const handleInputChange = (event) => {
@@ -96,15 +125,22 @@ function ChangeInfo(props) {
 
         return (
         <div>
-            <img src = {img} width={'100px'} height={'100px'} alt='프로필 이미지'/>
-            <input type="file" id="file" onChange={handleChangeFile}></input>
-            <input
-                type="text"
-                placeholder="새로운 닉네임을 입력하세요"
-                value={nickname}
-                onChange={handleInputChange}
-            />
-            <button onClick={handleChangeNickname}>정보 변경</button>
+            <div className='image-update'>
+                <img className='image' src={img.url} alt='프로필 이미지'/>
+                <input className='image-input' type="file" id="file" ref={imageInputRef} onChange={handleChangeFile}></input>
+                <button onClick={onClickImageUpload}>이미지 선택</button>
+                <button className='image-button' onClick={() => {sendImg(img.tempData)}}>프로필 이미지 변경</button>                
+            </div>
+            <div className='nickname-update'>
+                <input
+                    className='nickname-input'
+                    type="text"
+                    placeholder="새로운 닉네임을 입력하세요"
+                    value={nickname}
+                    onChange={handleInputChange}
+                />
+                <button className='nickname-button' onClick={handleChangeNickname}>정보 변경</button>
+            </div>
         </div>
     );
 }
