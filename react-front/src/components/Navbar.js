c894fb99957df1509ccb788e5a4a4484d14fbbf6
202010823 <aayy4321l@naver.com>
2023-09-03 19:07:11 +0900

    navarcss

--- react-front/src/components/Navbar.js
+++ react-front/src/components/Navbar.js
@@ -11,15 +11,17 @@ function Navbar() {
     let category = pathname;
 
     return (
-        <div className='navbar-container'>
-            <div className='navbar'>
-                <span className='navbarline'>|</span>
-                <a className={`navbarMenu ${category === '/' ? 'selected' : 'unselected'}`} href={'/'}>Home</a>
-                <span className='navbarline'>|</span>
-                <a className={`navbarMenu ${category.includes('/userRank') ? 'selected' : 'unselected'}`} href={'/userRank/4-major'}>User ranking</a>
-                <span className='navbarline'>|</span>
-                <a className={`navbarMenu ${category.includes('/fitnessRank') ? 'selected' : 'unselected'}`} href={'/fitnessRank'}>Exercise</a>    
-                <span className='navbarline'>|</span>
+        <div className='navbar-back'>
+            <div className='navbar-container'>
+                <div className='navbar'>
+                    <span className='navbarline'>|</span>
+                    <a className={`navbarMenu ${category === '/' ? 'selected' : 'unselected'}`} href={'/'}>Home</a>
+                    <span className='navbarline'>|</span>
+                    <a className={`navbarMenu ${category.includes('/userRank') ? 'selected' : 'unselected'}`} href={'/userRank/4-major'}>User ranking</a>
+                    <span className='navbarline'>|</span>
+                    <a className={`navbarMenu ${category.includes('/fitnessRank') ? 'selected' : 'unselected'}`} href={'/fitnessRank'}>Exercise</a>    
+                    <span className='navbarline'>|</span>
+                </div>
             </div>
         </div>
     );
