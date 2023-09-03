19d2559798e69503d66b841c0f021903feb53cc2
hjinshin <gudwls818@gmail.com>
2023-09-04 04:34:38 +0900

    CORS를 클라이언트만 허용

--- springboot-back/src/main/java/com/health/springbootback/controller/DataController.java
+++ springboot-back/src/main/java/com/health/springbootback/controller/DataController.java
@@ -18,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 public class DataController {
 
--- springboot-back/src/main/java/com/health/springbootback/controller/RankingController.java
+++ springboot-back/src/main/java/com/health/springbootback/controller/RankingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 public class RankingController {
     private final RankingService rankingService;
