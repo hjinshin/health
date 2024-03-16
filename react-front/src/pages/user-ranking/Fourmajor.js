66d104e5815a3ee494634d32e16b6fc709615054
hjinshin <gudwls818@gmail.com>
2024-03-17 00:07:44 +0900

    console.log 삭제

--- react-front/src/pages/user-ranking/Fourmajor.js
+++ react-front/src/pages/user-ranking/Fourmajor.js
@@ -14,7 +14,6 @@ function Fourmajor(props) {
         async function fetchSubCategories() {
             let tempButton = [{id: 1, label: "합계", type: "SUM"}];
             try {
-                console.log(process.env.REACT_APP_SPRINGBOOT_BACK_URL);
                 const res = await axios.get(SERVER_SEARCH_URL + '/api/subcategory?cid=FOURMAJOR');
                 res.data.forEach((subcategory, index) => {
                     tempButton.push({
